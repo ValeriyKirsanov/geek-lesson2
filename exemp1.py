5a1fa50d6fc248af2fcc5c89b8a739bbfe29f32b
ValeriyKirsanov <70278649+ValeriyKirsanov@users.noreply.github.com>
2020-08-31 20:19:21 +0300

    Add files via upload

---
+++ exemp1.py
@@ -0,0 +1,12 @@
+a = 5
+numbers = int(a)
+print(numbers)
+b = "name"
+numbers2 = list(b)
+print(numbers2)
+c = tuple(b)
+print(c)
+d = frozenset(c)
+print(d)
+i = dict(key_1='a', key_2='b')
+print(i)
